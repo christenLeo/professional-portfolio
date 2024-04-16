04ecba367c6c03688d4e7600dea140b55191d515
Leona Christen <leo.christenmacedo@gmail.com>
2024-04-16 09:13:51 -0300

    chore: Adding config.ts constants fallback values

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const ACCESS_TOKEN = process.env.GITHUB_PERSONAL_ACCESS_TOKEN;
-const USERNAME = process.env.GITHUB_USERNAME;
-const GITHUB_URI = process.env.GITHUB_URI;
+const ACCESS_TOKEN = process.env.GITHUB_PERSONAL_ACCESS_TOKEN || "";
+const USERNAME = process.env.GITHUB_USERNAME || "";
+const GITHUB_URI = process.env.GITHUB_URI || "https://api.github.com/graphql";
 
 export { ACCESS_TOKEN, USERNAME, GITHUB_URI };
