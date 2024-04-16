cb374f39663f8f7a99231469043b69f5bfb0345d
Leona Christen <leo.christenmacedo@gmail.com>
2024-04-16 08:51:09 -0300

    chore: adding config file with constants

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,14 +1,11 @@
+import { ACCESS_TOKEN, GITHUB_URI, USERNAME } from "@/config";
 import { NextResponse } from "next/server";
-import dotenv from "dotenv";
 
-dotenv.config();
 
 export async function GET() {
-  const ACESS_TOKEN = process.env.GITHUB_PERSONAL_ACCESS_TOKEN;
-  const USER_NAME = process.env.GITHUB_USERNAME;
   const reqBodyContent = {
     query: `{
-        user(login: "${USER_NAME}") {
+        user(login: "${USERNAME}") {
           pinnedItems(first: 6, types: REPOSITORY) {
             nodes {
               ... on Repository {
@@ -26,13 +23,13 @@ export async function GET() {
   const reqConfig = {
     method: "POST",
     headers: {
-      Authorization: `Bearer ${ACESS_TOKEN}`,
+      Authorization: `Bearer ${ACCESS_TOKEN}`,
       "Content-type": "application/json",
     },
     body: JSON.stringify(reqBodyContent),
   };
 
-  const {data} = await fetch("https://api.github.com/graphql", reqConfig).then((data) => {
+  const {data} = await fetch(`${GITHUB_URI}`, reqConfig).then((data) => {
     return data.json();
   });
   return NextResponse.json({ data });
---
+++ src/config.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const ACCESS_TOKEN = process.env.GITHUB_PERSONAL_ACCESS_TOKEN;
+const USERNAME = process.env.GITHUB_USERNAME;
+const GITHUB_URI = process.env.GITHUB_URI;
+
+export { ACCESS_TOKEN, USERNAME, GITHUB_URI };
